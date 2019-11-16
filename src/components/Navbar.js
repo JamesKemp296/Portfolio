51f57960efd9752204e9afb75cbf70ac94c24159
JamesKemp296 <jameskemp2796@yahoo.com>
2019-11-16 09:38:11 -0500

    navbar styling is 90% and focus can be moved to the body

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,12 @@ import React from 'react'
 import styles from './Navbar.module.css'
 import { NavLink, Link } from 'react-router-dom'
 import logo from '../assets/images/badger128.png'
-// import { AiOutlineHome } from 'react-icons/ai'
-// import { AiOutlineUser } from 'react-icons/ai'
-// import { AiOutlineSetting } from 'react-icons/ai'
-// import { AiOutlineProject } from 'react-icons/ai'
-// import { AiOutlineMail } from 'react-icons/ai'
-// import { AiOutlineTwitter } from 'react-icons/ai'
-// import { AiFillLinkedin } from 'react-icons/ai'
-// import { AiOutlineGithub } from 'react-icons/ai'
 
 const Navbar = () => {
-
   return (
     <header id={styles.header}>
       <div id={styles.headerGroup}>
-        <div className={styles.headerGroupLeft}>
+        <div id={styles.logoGroup}>
           <Link>
             <img src={logo} alt="badger" className={styles.logo}/>
           </Link>
@@ -25,16 +16,14 @@ const Navbar = () => {
             <h2>Full-Stack Developer</h2>
           </div>
         </div>
-        <div className={styles.headerGroupRight}>
-          <nav>
-            <ul className={styles.navbar}>
-              <li><NavLink to="/About">About</NavLink></li>
-              <li><NavLink exact to="/">Portfolio</NavLink></li>
-              <li><NavLink to="/skills">Skills</NavLink></li>
-              <li><NavLink to="/contact">Contact</NavLink></li>
-            </ul>
-          </nav>
-        </div>
+        <nav className={styles.navbar}>
+          <ul>
+            <li><NavLink to="/About">About</NavLink></li>
+            <li><NavLink exact to="/">Portfolio</NavLink></li>
+            <li><NavLink to="/skills">Skills</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+        </nav>
       </div>
     </header>
   )
