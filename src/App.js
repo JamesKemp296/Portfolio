9daf85730412418660ffe3502286b66d82e9842f
JamesKemp296 <jameskemp2796@yahoo.com>
2019-11-15 22:20:21 -0500

    removing home component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
-import Home from './pages/Home'
 import About from './pages/About'
 import Skills from './pages/Skills'
 import Portfolio from './pages/Portfolio'
@@ -13,10 +12,9 @@ function App() {
     <BrowserRouter>
       <Navbar/>
       <Switch>
-        <Route exact path="/" component={Home}/>
         <Route path="/about" component={About} />
-        <Route path="/skills" component={Skills} />
         <Route path="/portfolio" component={Portfolio} />
+        <Route path="/skills" component={Skills} />
         <Route path="/contact" component={Contact} />
       </Switch>
     </BrowserRouter>
