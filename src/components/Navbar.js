fa5cf4f5853aa9d6d7c859d5421cae9b1e3b5335
JamesKemp296 <jameskemp2796@yahoo.com>
2019-11-15 23:42:59 -0500

    Working on nav, prcticing css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,42 @@
 import React from 'react'
 import styles from './Navbar.module.css'
-import { NavLink } from 'react-router-dom'
-import { AiOutlineHome } from 'react-icons/ai'
-import { AiOutlineUser } from 'react-icons/ai'
-import { AiOutlineSetting } from 'react-icons/ai'
-import { AiOutlineProject } from 'react-icons/ai'
-import { AiOutlineMail } from 'react-icons/ai'
-import { AiOutlineTwitter } from 'react-icons/ai'
-import { AiFillLinkedin } from 'react-icons/ai'
-import { AiOutlineGithub } from 'react-icons/ai'
+import { NavLink, Link } from 'react-router-dom'
+import logo from '../assets/images/badger128.png'
+// import { AiOutlineHome } from 'react-icons/ai'
+// import { AiOutlineUser } from 'react-icons/ai'
+// import { AiOutlineSetting } from 'react-icons/ai'
+// import { AiOutlineProject } from 'react-icons/ai'
+// import { AiOutlineMail } from 'react-icons/ai'
+// import { AiOutlineTwitter } from 'react-icons/ai'
+// import { AiFillLinkedin } from 'react-icons/ai'
+// import { AiOutlineGithub } from 'react-icons/ai'
 
 const Navbar = () => {
 
   return (
-    <div className={styles.navbar}>
-     <div className={styles.logo}>J</div>
-     <nav>
-        <ul className={styles.navbarLinks}>
-          <li><NavLink exact to="/"><AiOutlineHome /></NavLink></li>
-          <li><NavLink to="/About"><AiOutlineUser /></NavLink></li>
-          <li><NavLink to="/skills"><AiOutlineSetting /></NavLink></li>
-          <li><NavLink to="/portfolio"><AiOutlineProject /></NavLink></li>
-          <li><NavLink to="/contact"><AiOutlineMail /></NavLink></li>
-        </ul>
-      </nav>
-      <ul className={styles.socialLinks}>
-        <li><AiOutlineTwitter /></li>
-        <li><AiFillLinkedin /></li>
-        <li><AiOutlineGithub /></li>
-      </ul>
-    </div>
+    <header id={styles.header}>
+      <div id={styles.headerGroup}>
+        <div className={styles.headerGroupLeft}>
+          <Link>
+            <img src={logo} alt="badger" className={styles.logo}/>
+          </Link>
+          <div className={styles.logoName}>
+            <h1>James Kemp</h1>
+            <h2>Full-Stack Developer</h2>
+          </div>
+        </div>
+        <div className={styles.headerGroupRight}>
+          <nav>
+            <ul className={styles.navbar}>
+              <li><NavLink to="/About">About</NavLink></li>
+              <li><NavLink exact to="/">Portfolio</NavLink></li>
+              <li><NavLink to="/skills">Skills</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
   )
 }
 
